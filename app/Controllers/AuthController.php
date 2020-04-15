ee850762372eab1f03bdab26cb1847563d88d476
andresrueda90 <andresrueda90@gmail.com>
2020-04-15 12:44:00 -0500

    commit final de ajustes

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends BaseController {
         if($user) {
             if (password_verify($postData['password'], $user->password)) {
                 $_SESSION['userId'] = $user->id;
-                return new RedirectResponse('/admin');
+                return new RedirectResponse('admin');
             } else {
                 $responseMessage = 'Bad credentials';
             }
@@ -34,6 +34,6 @@ class AuthController extends BaseController {
 
     public function getLogout() {
         unset($_SESSION['userId']);
-        return new RedirectResponse('/login');
+        return new RedirectResponse('login');
     }
 }
--- script_DB.sql
+++ script_DB.sql
@@ -16,6 +16,15 @@ CREATE TABLE`cursophp`.`jobs` (
   `months`INT NULL,
   PRIMARY KEY (`id`));
 
+  CREATE TABLE `cursophp`.`users` (
+     `id` INT(10) NOT NULL AUTO_INCREMENT ,
+      `email` VARCHAR(200) NOT NULL ,
+       `password` VARCHAR(200) NOT NULL ,
+        `created_at` DATETIME NOT NULL ,
+         PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+  ALTER TABLE `jobs` ADD `image` VARCHAR(500)  NOT NULL AFTER `months`;
   ALTER TABLE `jobs` ADD `created_at` DATETIME NOT NULL AFTER `months`, ADD `updated_at` DATETIME NOT NULL AFTER `created_at`;
   ALTER TABLE `proyects` ADD `created_at` DATETIME NOT NULL AFTER `months`, ADD `updated_at` DATETIME NOT NULL AFTER `created_at`;
   ALTER TABLE `jobs` ADD `deleted_at` DATETIME NOT NULL AFTER `updated_at`;
+  ALTER TABLE `users` ADD `updated_at` DATETIME NOT NULL AFTER `created_at`;
